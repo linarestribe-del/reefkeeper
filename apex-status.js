307947e71d25937b908aec0d3ce94fd80c2ec5e0
linarestribe-del <linarestribe@gmail.com>
2026-07-01 20:55:28 -0700

    Add files via upload

---
+++ apex-status.js
@@ -0,0 +1,20 @@
+import { latestApexStatus } from './apex-sync.js';
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  if (!latestApexStatus) {
+    return res.status(404).json({
+      ok: false,
+      error: 'No Apex status has been received yet'
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+    ...latestApexStatus,
+    status: latestApexStatus
+  });
+}
